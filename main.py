9bcd8bd5357765297664bfd6b1bc279daa67149d
Vladimir Smetana <smetanavladimir7@gmail.com>
2025-05-25 20:56:13 +0300

    overview

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ print(data.info())
 
 # Статистическое описание
 print(data.describe())
+
+# plt.figure(figsize=(10, 6))
+# plt.scatter(data['LEO'], data['FullMass'])
+# plt.title('Распределение полезной нагрузки НОО')
+# plt.xlabel('FullMass')
+# plt.ylabel('LEO')
+# plt.grid()
+# plt.show()
+
+overview_headers = ['Fuel', 'Country', 'Class', 'Purpose', 'Activity']
+for header in overview_headers:
+    print(f'Заголовок: {header}')
+    unique_values = data[header].value_counts()
+    print(unique_values)
+    print()
+
